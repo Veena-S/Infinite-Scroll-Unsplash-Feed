18eb91ace6a6d438df701be370cd41d7f468efed
Veena-S <veenasivan@gmail.com>
2021-04-26 21:08:47 +0800

    Removed unwanted include

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 // import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text, View, StatusBar } from 'react-native';
+import { Text, View } from 'react-native';
 import FeedList from "./src/components/FeedList.jsx"
 import styles from "./src/Styles.js"
 import {UnsplashFeedProvider} from "./src/appContextStore.jsx"
